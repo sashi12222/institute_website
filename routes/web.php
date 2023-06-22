4a050b8039db5570790e1e95b0a88eb9defc6477
sashi12222 <sashiraj1222@gmail.com>
2023-06-22 22:55:11 +0530

    today xampp not working

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\AboutController;
+use App\Http\Controllers\Admin\BlogController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\CourseController;
 use App\Http\Controllers\Admin\SettingController;
@@ -35,3 +36,4 @@ Route::resource('about',AboutController::class);
 Route::resource('category',CategoryController::class);
 Route::resource('course',CourseController::class);
 Route::resource('upcoming',UpcomingController::class);
+Route::resource('blog',BlogController::class);
